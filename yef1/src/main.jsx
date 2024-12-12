a820d69872b0ef3664630a053d1061cc8450002d
Aezrael1 <arjundikshit23@gmail.com>
2024-12-12 22:38:59 +0530

    fixed some changes

--- yef1/src/main.jsx
+++ yef1/src/main.jsx
@@ -3,7 +3,12 @@ import { createRoot } from "react-dom/client";
 import Login from "./Login/Login.jsx";
 import Signup from "./Signup/Signup.jsx";
 import Home from "./home.jsx";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import { AuthProvider } from "./contexts/authContext/index.jsx";
 
 createRoot(document.getElementById("root")).render(
@@ -11,6 +16,7 @@ createRoot(document.getElementById("root")).render(
     <AuthProvider>
       <Router>
         <Routes>
+          <Route path="/" element={<Navigate to="/login" replace />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/home" element={<Home />} />
